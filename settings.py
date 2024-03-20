9b8e22fa7bcdc0390579a24d232e9f237be8c024
Thaveesha Sonnadara <sonnadarathaveesha@gmail.com>
2024-03-21 01:20:36 +0530

    removed PORT variable

--- settings.py
+++ settings.py
@@ -2,5 +2,5 @@ from os import environ
 
 ROBOFLOW_API_KEY = environ.get('ROBOFLOW_API_KEY')
 FLASK_DEBUG = environ.get('FLASK_DEBUG')
-PORT = environ.get('PORT')
 TF_ENABLE_ONEDNN_OPTS = environ.get('TF_ENABLE_ONEDNN_OPTS')
+FLASK_RUN_PORT = environ.get('FLASK_RUN_PORT')
