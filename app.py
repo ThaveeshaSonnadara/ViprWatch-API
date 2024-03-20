daf4477a7a79c96e2ec526d942d711337da19c7c
Thaveesha Sonnadara <sonnadarathaveesha@gmail.com>
2024-03-21 00:25:31 +0530

    added app.py file which contains the API code

---
+++ app.py
@@ -0,0 +1,70 @@
+import numpy as np
+from PIL import Image
+from flask import Flask, jsonify, request
+from keras.models import load_model
+from inference import get_roboflow_model
+from dotenv import load_dotenv
+from flask_cors import CORS
+
+app = Flask(__name__)
+
+CORS(app)
+
+app.config.from_pyfile('settings.py')
+
+model_phase1 = get_roboflow_model(model_id="snake-detection/2")
+model_phase2 = load_model('model_for_10snakes.h5')
+
+
+@app.route('/', methods=['POST'])
+def index():
+    if 'file' not in request.files:
+        return jsonify(error="Please try again. The image doesn't exist.")
+
+    # Get image from POST request
+    file = request.files.get('file')
+    img_bytes = file.read()
+    img_path = "./uploadImages/test.jpg"
+    with open(img_path, "wb") as img:
+        img.write(img_bytes)
+
+    results = model_phase1.infer(img_path, confidence=0.25)
+    print(results)
+
+    if len(results) > 0 and len(results[0].predictions) > 0 and results[0].predictions[0].class_name == 'Snake':
+        # Load image and convert to NumPy array
+        img = Image.open(img_path)
+        img = img.resize((224, 224))
+        img_array = np.array(img)
+        img_array = np.expand_dims(img_array, axis=0)
+        img_array = img_array.astype('float32') / 255.
+
+        # Make prediction
+
+        prediction = model_phase2.predict(img_array)
+        print(prediction)
+
+        # Convert prediction to class label and confidence level
+        predicted_class = np.argmax(prediction)
+        confidence_level = np.max(prediction)
+
+        # Return JSON response
+
+        if confidence_level >= 0.80:
+            response = {'predicted_class': int(predicted_class), 'confidence_level': float(confidence_level)}
+            print(response)
+            return jsonify(response)
+        else:
+            response = {'predicted_class': -2, 'confidence_level': 0.05}
+            print(response)
+            return jsonify(response)
+
+    else:
+        response = {'predicted_class': -1, 'confidence_level': 0.05}
+        print(response)
+        return jsonify(response)
+
+
+if __name__ == '__main__':
+    load_dotenv()
+    app.run()
